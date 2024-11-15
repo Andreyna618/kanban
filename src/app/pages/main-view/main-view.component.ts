2ff1d6bb511b8d0c451aefc162928443c6e37c5d
Andreyna618 <andreynagb@hotmail.com>
2024-11-15 20:15:01 -0300

    project_done

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -18,41 +18,43 @@ import { Column } from '../../models/column.model';
   standalone: true,
   imports: [CdkDrag, CdkDropList, DragDropModule, CommonModule],
   templateUrl: './main-view.component.html',
-  styleUrl: './main-view.component.scss'
+  styleUrl: './main-view.component.scss',
 })
+//Definição das tarefas
 export class MainViewComponent {
-  board:Board= new Board('Test Board', [
-    new Column('Tarefas',[
-      "Desenvolver BackEnd",
-      "Desenvolver FrontEnd",
-      "Configuração do ambiente de desenvolvimento "
+  board: Board = new Board('Test Board', [
+    new Column('Tarefas', [
+      'Desenvolver BackEnd',
+      'Desenvolver FrontEnd',
+      'Configuração do ambiente de desenvolvimento ',
     ]),
-    new Column('Desenvolvendo',[
-      "Ajustar Colunas",
-      "Ajustar Título",
-      "Aplicar Imagem de fundo"
+    new Column('Desenvolvendo', [
+      'Ajustar Colunas',
+      'Ajustar Título',
+      'Aplicar Imagem de fundo',
     ]),
     new Column('Dificuldades', [
       'Importações',
-       'Aplicar opacidade', 
-       'Implementar método Drag e Drop'
-      ]),
-      new Column('Concluído', ['Projeto Kanban']),
-
+      'Aplicar opacidade',
+      'Implementar método Drag e Drop',
+    ]),
+    new Column('Concluído', ['Projeto Kanban']),
   ]);
-
-  
-  
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,);
+        event.currentIndex
+      );
     }
   }
 }
